03926f8cd62ebf45c95c2e8956c5b9a7b47dd151
Phạm Dương Thúy Hằng <hang33423@gmail.com>
2025-09-20 21:05:58 +0700

    Add files via upload

---
+++ Tuan4_Bai2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Tuan4_Bai2 {
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Nhap so a: ");
+        int a = sc.nextInt();
+
+        System.out.print("Nhap so b: ");
+        int b = sc.nextInt();
+
+        System.out.print("Nhap so c: ");
+        int c = sc.nextInt();
+
+        int min = a; 
+        if (b < min) min = b;
+        if (c < min) min = c;
+
+        System.out.println("So nhat nhat la: " + min);
+    }
+}
